e23255107163c90a44835d124f85bfccd2a4282a
ELz0N1 <ELz0N1@users.noreply.github.com>
2024-09-10 04:37:50 +0000

    Deploying to gh-pages from @ ELz0N1/OOP@122ac5974db5e02e27321cd1b907977f46b2da91 🚀

--- Task_1_1_1/member-search-index.js
+++ Task_1_1_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"ru.nsu","c":"HeapSort","l":"HeapSort()","url":"%3Cinit%3E()"}]
+memberSearchIndex = [{"p":"ru.nsu","c":"HeapSort","l":"HeapSort()","url":"%3Cinit%3E()"},{"p":"ru.nsu","c":"HeapSort","l":"main(String[])","url":"main(java.lang.String[])"}]

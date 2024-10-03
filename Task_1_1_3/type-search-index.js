34b7c4dde7223cb26f06bd73a40804ab5f7b0003
ELz0N1 <ELz0N1@users.noreply.github.com>
2024-10-03 16:12:42 +0000

    Deploying to gh-pages from @ ELz0N1/OOP@5ecd48163cb799100f6c89b1c6940bb06e388391 🚀

---
+++ Task_1_1_3/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"ru.nsu","l":"Add"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu","l":"Expression"},{"p":"ru.nsu","l":"Main"},{"p":"ru.nsu","l":"Mul"},{"p":"ru.nsu","l":"Sub"},{"p":"ru.nsu","l":"Variable"}]

c52a287b7ed1d91aba4a5375b314acf86ee30305
ELz0N1 <ELz0N1@users.noreply.github.com>
2024-11-25 17:08:24 +0000

    Deploying to gh-pages from @ ELz0N1/OOP@3c93f6015875ba075e7a2684a1197ceb1e73391b 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu","l":"ControlType"},{"p":"ru.nsu","l":"CourseGrade"},{"p":"ru.nsu","l":"Grade"},{"p":"ru.nsu","l":"StudentGradeBook"}]
